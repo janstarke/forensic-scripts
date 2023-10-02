87dad9f2e5c16d66858f771ed25f1e8b4379f48b
Jan Starke <jan.starke@telekom.de>
2023-10-02 13:49:40 +0200

    change sort_by to timestamp

--- windows-timeline/tlnobjects/exportable_item.py
+++ windows-timeline/tlnobjects/exportable_item.py
@@ -5,14 +5,14 @@ import csv
 class ExportableItem(object):
     def __init__(self,
                  attributes: list,
-                 sort_by: str):
-        if sort_by not in attributes:
-            raise SyntaxError(f"cannot sort by {sort_by}, because it is not an attribute")
-        self.__sort_by = sort_by
+                 timestamp: str):
+        if timestamp not in attributes:
+            raise SyntaxError(f"cannot sort by {timestamp}, because it is not an attribute")
+        self.__timestamp = timestamp
         self.__attributes = attributes
 
     def __call__(self, cls):
-        cls.sort_by_attribute = staticmethod(lambda: self.__sort_by)
+        cls.timestamp_attribute = staticmethod(lambda: self.__timestamp)
         cls.attributes = staticmethod(lambda: self.__attributes)
         return cls
 
@@ -21,14 +21,14 @@ class AbstractExportableItem(object, metaclass=ABCMeta):
     def __init__(self, entries: list):
         self.__entries = entries
 
-    def sort_by_attribute(self) -> str:
+    def timestamp_attribute(self) -> str:
         pass
 
     def attributes(self) -> list:
         pass
 
     def to_csv(self, dst_file):
-        self.__entries.sort(key=lambda e: e.__getattribute__(self.sort_by_attribute()))
+        self.__entries.sort(key=lambda e: e.__getattribute__(self.timestamp_attribute()))
         writer = csv.DictWriter(dst_file, fieldnames=self.attributes())
 
         for entry in self.__entries:
--- windows-timeline/tlnobjects/userassist.py
+++ windows-timeline/tlnobjects/userassist.py
@@ -3,9 +3,9 @@ from .exportable_item import ExportableItem, AbstractExportableItem
 
 
 @ExportableItem(
-    attributes=["ts", "hostname", "domain", "path", "number_of_executions", "application_focus_count", "regf_hive_path",
-                "user_id", "username"],
-    sort_by="ts"
+    attributes=["ts", "username", "path", "hostname", "domain", "number_of_executions", "application_focus_count", "regf_hive_path",
+                "user_id"],
+    timestamp="ts"
 )
 class UserAssist(AbstractExportableItem):
     def __init__(self, t: Target):
